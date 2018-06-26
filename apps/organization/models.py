ee18360d4b3b7d6c4f15872ad9911d31f138e82d
Jason_Sam1996 <1301954425@qq.com>
2018-06-26 18:50:52 +0800

    第一次提交

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import datetime
+
+
 # Create your models here.
 
 class CityDict(models.Model):
@@ -11,29 +13,28 @@ class CityDict(models.Model):
         verbose_name = u"城市"
         verbose_name_plural = verbose_name
 
-class CourseOrg(models.Model):
 
-    name = models.CharField(max_length=50,verbose_name=u'机构名称')
-    desc = models.CharField(max_length=200,verbose_name=u'机构描述')
-    chick_nums = models.IntegerField(default=0,verbose_name=u'点击信息')
-    fav_nums = models.IntegerField(default=0,verbose_name=u'收藏信息')
-    image = models.ImageField(upload_to='org/%Y/%m',max_length=100,verbose_name=u'机构封面')
-    address = models.CharField(max_length=100,verbose_name=u'机构地址')
-    city = models.ForeignKey(CityDict,verbose_name=u'所在城市')
+class CourseOrg(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'机构名称')
+    desc = models.CharField(max_length=200, verbose_name=u'机构描述')
+    chick_nums = models.IntegerField(default=0, verbose_name=u'点击信息')
+    fav_nums = models.IntegerField(default=0, verbose_name=u'收藏信息')
+    image = models.ImageField(upload_to='org/%Y/%m', max_length=100, verbose_name=u'机构封面')
+    address = models.CharField(max_length=100, verbose_name=u'机构地址')
+    city = models.ForeignKey(CityDict, verbose_name=u'所在城市')
     add_time = models.DateTimeField(default=datetime.now)
 
     class Meta:
         verbose_name = u'机构信息'
         verbose_name_plural = verbose_name
 
+
 class Teacher(models.Model):
-
-    org = models.ForeignKey(CourseOrg,verbose_name=u'所在机构')
-    name = models.CharField(max_length=50,verbose_name=u'教师名称')
-    work_year = models.IntegerField(default=1,verbose_name=u'工作年龄')
-    work_company = models.CharField(max_length=50,verbose_name=u'就职公司')
-    work_position = models.CharField(max_length=50,verbose_name=u'工作职位')
-
+    org = models.ForeignKey(CourseOrg, verbose_name=u'所在机构')
+    name = models.CharField(max_length=50, verbose_name=u'教师名称')
+    work_year = models.IntegerField(default=1, verbose_name=u'工作年龄')
+    work_company = models.CharField(max_length=50, verbose_name=u'就职公司')
+    work_position = models.CharField(max_length=50, verbose_name=u'工作职位')
 
     class Meta:
         verbose_name = u'教师信息'
